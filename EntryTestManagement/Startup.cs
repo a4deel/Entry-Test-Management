ce516d1212a0b0da87c108812aa0cc7198f75969
a4deel <aa4deel@gmail.com>
2021-03-05 21:40:57 +0500

    Add project files.

---
+++ EntryTestManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EntryTestManagement.Startup))]
+namespace EntryTestManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
