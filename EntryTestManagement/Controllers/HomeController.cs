dc1fd60ac01cfb5029bd2d31fe7a52175f6db90c
a4deel <aa4deel@gmail.com>
2021-03-06 22:56:05 +0500

    Admin Login Module Finished

--- EntryTestManagement/Controllers/HomeController.cs
+++ EntryTestManagement/Controllers/HomeController.cs
@@ -1,30 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using EntryTestManagement.Models;
 
 namespace EntryTestManagement.Controllers
 {
     public class HomeController : Controller
     {
+        EntryTestDetailsDBEntities1 DataStorage = new EntryTestDetailsDBEntities1();
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult UserLogin()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult AdminLogin()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AdminLogin(String email,String password)
+        {
+            //if (ModelState.IsValid)
+            //{
+                var foundAdmin = DataStorage.AdminLogins.Where(obj => obj.email.Equals(email) && obj.password.Equals(password)).FirstOrDefault();
+                if(foundAdmin != null)
+                {
+                    Session["AdminEmail"] = foundAdmin.email.ToString();
+                    return RedirectToAction("Dashboard");
+                }
+                else
+                {
+                    TempData["Error"] = "(Incorrect email/password)";
+                    return RedirectToAction("AdminLogin");
+                }
+            //}
+           
+        }
+
+        public ActionResult RegisterUser()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            if(Session["AdminEmail"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("AdminLogin");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index");
         }
     }
 }
